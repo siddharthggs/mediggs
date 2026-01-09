b32223474561756214d59cd2b90a30104af04d37
siddharthggs <ytproject6969@gmail.com>
2026-01-09 18:32:54 +0530

    Delete prisma directory

--- prisma/migrations/20251203040914_add_state_fields/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "CompanyInfo" ADD COLUMN "state" TEXT;
-ALTER TABLE "CompanyInfo" ADD COLUMN "stateCode" TEXT;
--- prisma/migrations/20251205141350_add_customer_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Customer" ADD COLUMN "drugLicenseNumber2" TEXT;
-ALTER TABLE "Customer" ADD COLUMN "panNumber" TEXT;
-ALTER TABLE "Customer" ADD COLUMN "stateCode" TEXT;
